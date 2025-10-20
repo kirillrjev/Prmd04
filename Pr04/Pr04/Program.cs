e85e7c944856b58a21d23a76c7e7831245c865df
kirillrjev <kirillrjev@gmail.com>
2025-10-20 16:10:08 +0300

    Add files via upload

---
+++ Pr04/Pr04/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace BookPractice
+{
+    public class Book
+    {
+        private string title;
+        private string author;
+        private int year;
+        private int pages;
+
+        public string Title
+        {
+            get => title;
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                    title = value;
+            }
+        }
+
+        public string Author
+        {
+            get => author;
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                    author = value;
+            }
+        }
+
+        public int Year
+        {
+            get => year;
+            set
+            {
+                int currentYear = DateTime.Now.Year;
+                if (value > 1400 && value <= currentYear)
+                    year = value;
+            }
+        }
+
+        public int Pages
+        {
+            get => pages;
+            set
+            {
+                if (value > 0)
+                    pages = value;
+            }
+        }
+        public Book()
+        {
+            title = "Неизвестно";
+            author = "Неизвестен";
+            year = DateTime.Now.Year;
+            pages = 100;
+        }
+
+        public Book(string title, string author, int year, int pages)
+        {
+            Title = title;
+            Author = author;
+            Year = year;
+            Pages = pages;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Книга: {Title}");
+            Console.WriteLine($"Автор: {Author}");
+            Console.WriteLine($"Год издания: {Year}");
+            Console.WriteLine($"Страниц: {Pages}");
+            Console.WriteLine($"Старая книга: {(IsOld() ? "Да" : "Нет")}");
+        }
+
+        public bool IsOld()
+        {
+            return DateTime.Now.Year - year > 50;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book book1 = new Book("Преступление и наказание", "Фёдор Достоевский", 1866, 671);
+            book1.DisplayInfo();
+
+            Console.WriteLine();
+
+            Book book2 = new Book();
+            book2.Title = "C# для начинающих";
+            book2.Author = "Ржевский Кирилл";
+            book2.Year = 2025;
+            book2.Pages = 350;
+            book2.DisplayInfo();
+        }
+    }
+}
